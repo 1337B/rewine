76c89ccc69e1c5e41d1da4fd9e4c6338644ecac8
Cristhian Bielaszczuk <cristhianbielaszczuk@cbielszczuk.local>
2026-01-12 13:13:50 -0300

    [#11][FEATURE] AI comparing cache and enhance ui

--- frontend/src/pages/public/HomePage.vue
+++ frontend/src/pages/public/HomePage.vue
@@ -70,7 +70,12 @@ const popularGrapes = [
           <div class="flex flex-wrap justify-center gap-4">
             <router-link to="/wines">
               <BaseButton size="lg" class="px-8">
-                Explore Wines
+                <img src="/images/icons/reshot-icon-red-wine-L2HFAY75WG.svg" alt="" class="w-5 h-5 mr-2 inline-block" />Explore Wines
+              </BaseButton>
+            </router-link>
+            <router-link to="/wines/scan">
+              <BaseButton variant="outline" size="lg" class="px-8 !border-wine-300 !text-white hover:!bg-wine-300/20 hover:!border-wine-200">
+                <svg class="w-5 h-5 mr-2 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" /></svg>Scan Wine
               </BaseButton>
             </router-link>
             <router-link v-if="!isAuthenticated" to="/register">
@@ -80,8 +85,7 @@ const popularGrapes = [
             </router-link>
             <router-link v-else to="/cellar">
               <BaseButton variant="outline" size="lg" class="px-8 !border-wine-300 !text-white hover:!bg-wine-300/20 hover:!border-wine-200">
-                <img src="/images/icons/reshot-icon-vine-cellar-PK3MZL62NG.svg" alt="" class="w-5 h-5 mr-2 invert" />
-                My Cellar
+                <img src="/images/icons/reshot-icon-vine-cellar-PK3MZL62NG.svg" alt="" class="w-5 h-5 mr-2 invert inline-block" />My Cellar
               </BaseButton>
             </router-link>
           </div>
